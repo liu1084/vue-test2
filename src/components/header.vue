71efa9af1243d0a28c423980a07400a6db49c4f2
jim <liu1084@163.com>
2017-05-19 19:09:02 +0800

    vue test init

---
+++ src/components/header.vue
@@ -0,0 +1,20 @@
+<template>
+	<header class="header" id="header">
+		<ul class="nav">
+			<li class="nav-tab">Home</li>
+			<li class="nav-tab">Article</li>
+			<li class="nav-tab">About</li>
+			<li class="nav-tab">Login</li>
+			<li class="nav-tab">Comments</li>
+		</ul>
+	</header>
+</template>
+
+<script>
+	export default  {
+		name: 'app',
+		data: function(){
+			return {};
+		}
+	}
+</script>
